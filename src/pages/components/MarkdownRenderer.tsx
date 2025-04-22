aba042f300376757f3170dcc151137189f841213
胖虎 <1424883981@qq.com>
2025-04-22 14:29:31 +0800

    🌈 style: 完善react-markdown样式

--- src/pages/components/MarkdownRenderer.tsx
+++ src/pages/components/MarkdownRenderer.tsx
@@ -19,47 +19,50 @@ interface ICodeProps extends React.HTMLAttributes<HTMLElement> {
 
 const MarkdownRenderer = ({ markdown }: IMarkdownRendererProps) => {
   return (
-    <ReactMarkdown
-      remarkPlugins={[remarkGfm]}
-      rehypePlugins={[rehypeColorChips]}
-      components={{
-        code({ node, inline, className, children, ...props }: ICodeProps) {
-          const match = /language-(\w+)/.exec(className || "");
-          return !inline && match ? (
-            <div className="bg-gray-100 p-8 rounded-4">
-              {/*  */}
-              <CopyToClipboard text={String(children).replace(/\n$/, "")}>
-                <div className="flex items-center justify-between py-4">
-                  <div className="lang">{match[1]}</div>
-                  <div className="cursor-pointer flex items-center">
-                    <CopyOutlined />
-                    <span className="ml-2">复制</span>
+    <div className="markdown-content">
+      <ReactMarkdown
+        remarkPlugins={[remarkGfm]}
+        rehypePlugins={[rehypeColorChips]}
+        components={{
+          // node：当前节点 inline：是否是行内代码块
+          code({ node, inline, className, children, ...props }: ICodeProps) {
+            const match = /language-(\w+)/.exec(className || "");
+            return !inline && match ? (
+              <div className="bg-gray-100 p-8 rounded-4">
+                {/*  */}
+                <CopyToClipboard text={String(children).replace(/\n$/, "")}>
+                  <div className="flex items-center justify-between py-4">
+                    <div className="lang">{match[1]}</div>
+                    <div className="cursor-pointer flex items-center">
+                      <CopyOutlined />
+                      <span className="ml-2">复制</span>
+                    </div>
                   </div>
-                </div>
-              </CopyToClipboard>
-              {/*  */}
-              <SyntaxHighlighter
-                {...props}
-                language={match[1]} // 高亮代码的语言
-                style={themeStyle}
-                PreTag="div" // 代替默认预标记的元素或自定义反应组件，即组件的最外层标记（
-                wrapLongLines={true} // 是否 <code> 使用 white-space: pre-wrap 或 来设置块的样式white-space: pre
-                wrapLines={false} // 用于确保每行代码在父元素中,false 时无法对行级别操作
-                showLineNumbers={true} // 代码行号
-              >
-                {String(children).replace(/\n$/, "")}
-              </SyntaxHighlighter>
-            </div>
-          ) : (
-            <code {...props} className={className}>
-              {children}
-            </code>
-          );
-        },
-      }}
-    >
-      {markdown}
-    </ReactMarkdown>
+                </CopyToClipboard>
+                {/*  */}
+                <SyntaxHighlighter
+                  {...props}
+                  language={match[1]} // 高亮代码的语言
+                  style={themeStyle}
+                  PreTag="div" // 代替默认预标记的元素或自定义反应组件，即组件的最外层标记（
+                  wrapLongLines={true} // 是否 <code> 使用 white-space: pre-wrap 或 来设置块的样式white-space: pre
+                  wrapLines={false} // 用于确保每行代码在父元素中,false 时无法对行级别操作
+                  showLineNumbers={true} // 代码行号
+                >
+                  {String(children).replace(/\n$/, "")}
+                </SyntaxHighlighter>
+              </div>
+            ) : (
+              <code {...props} className={className}>
+                {children}
+              </code>
+            );
+          },
+        }}
+      >
+        {markdown}
+      </ReactMarkdown>
+    </div>
   );
 };
 
