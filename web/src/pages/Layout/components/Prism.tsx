0e78915075ba459b6edc6a801908cae2bffc6b6c
胖虎 <1424883981@qq.com>
2025-05-06 14:25:56 +0800

    🌈 style: 代码高亮样式图标修改

--- web/src/pages/Layout/components/Prism.tsx
+++ web/src/pages/Layout/components/Prism.tsx
@@ -1,4 +1,4 @@
-import { DownOutlined } from "@ant-design/icons";
+import { CodeOutlined } from "@ant-design/icons";
 import { Dropdown, Space } from "antd";
 import PrismEnum from "@/enum/prism";
 import { useAppSelector, useAppDispatch } from "@/store";
@@ -7,7 +7,12 @@ import { setPrismTheme } from "@/store/modules/syntaxHighlighter";
 import type { MenuProps } from "antd";
 import type { TPrism } from "@/enum/prism";
 
-const items: MenuProps["items"] = Object.entries(PrismEnum).map(([_, value]) => ({ label: value, key: value }));
+const prismItems: MenuProps["items"] = Object.entries(PrismEnum).map(([_, value]) => ({ label: value, key: value }));
+const items: MenuProps["items"] = [
+  { label: <span className="font-bold text-[#333]">代码高亮样式</span>, key: "default", disabled: true },
+  { type: "divider" },
+  ...prismItems,
+];
 
 export const Prism = () => {
   const { prism } = useAppSelector(state => state.syntaxHighlighter);
@@ -17,10 +22,9 @@ export const Prism = () => {
   };
   return (
     <Dropdown menu={{ items, selectable: true, defaultSelectedKeys: [prism], onClick: handlePrismClick }}>
-      <a onClick={e => e.preventDefault()}>
+      <a className="cursor-pointer text-18 !text-black hover:!text-[#1890ff]" onClick={e => e.preventDefault()}>
         <Space>
-          代码高亮样式
-          <DownOutlined />
+          <CodeOutlined />
         </Space>
       </a>
     </Dropdown>
