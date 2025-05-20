5772f78a055bad50e82108f06de1233df04667d3
胖虎 <1424883981@qq.com>
2025-05-20 15:22:21 +0800

    ✨ feat: 前端添加获取用户聊天信息和消息历史记录的API接口

--- web/src/api/chat/index.ts
+++ web/src/api/chat/index.ts
@@ -1,4 +1,5 @@
 import request from "@/api/config";
+import type { IMessage } from "@/types/chat";
 
 export interface ICreateChatParams {
   chatId: number;
@@ -13,6 +14,11 @@ interface ICreateChatResponse {
   title: string;
 }
 
+interface IMessagesHistoryResponse extends ICreateChatResponse {
+  createDate: string;
+  messages: IMessage[];
+}
+
 /**
  * 创建聊天会话
  * @param body 聊天参数
@@ -20,4 +26,21 @@ interface ICreateChatResponse {
  */
 export const createChatApi = (body: ICreateChatParams) => {
   return request.post<ICreateChatParams, ICreateChatResponse>("/chat/createChat", body);
+};
+
+/**
+ * 获取用户所有聊天信息
+ * @returns 包含用户聊天信息的响应数组
+ */
+export const getUserChatInfosApi = () => {
+  return request.get<null, ICreateChatResponse[]>("/chat/userChatInfos");
+};
+
+/**
+ * 获取指定聊天ID的消息历史记录
+ * @param id 聊天ID
+ * @returns 包含消息历史记录的Promise对象
+ */
+export const getMessagesHistoryApi = (id: number) => {
+  return request.get<number, IMessagesHistoryResponse>(`/chat/messagesHistory?chatId=${id}`);
 };
