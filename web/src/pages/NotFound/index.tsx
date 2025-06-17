e214ee09c26776109e065ef458ba46c98c753d85
胖虎 <1424883981@qq.com>
2025-06-17 13:49:45 +0800

    ✨ feat: 优化404页面，添加首页跳转按钮

--- web/src/pages/NotFound/index.tsx
+++ web/src/pages/NotFound/index.tsx
@@ -1,8 +1,25 @@
+import { Button } from "antd";
+import { useNavigate } from "react-router-dom";
 
 const NotFound = () => {
-  return (
-    <div>404 - NotFound</div>
-  )
-}
+  const navigate = useNavigate();
 
-export default NotFound
+  /**
+   * 处理跳转到首页
+   * @description 点击按钮后，跳转到首页
+   */
+  const handleGoHome = () => {
+    navigate("/home");
+  };
+
+  return (
+    <div className="flex flex-col items-center justify-center h-screen">
+      <div className="text-30 font-bold">404 - NotFound</div>
+      <Button type="primary" className="!mt-20" onClick={handleGoHome}>
+        去首页
+      </Button>
+    </div>
+  );
+};
+
+export default NotFound;
